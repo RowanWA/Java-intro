8017bdec4501930d8e654508394d235902b88926
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2024-10-10 16:15:35 +0100

    dfsdf

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const myButton = document.getElementById("button")
 const myParagraph = document.getElementById("my-paragraph")
+const characterImg = document.getElementById("character-img")
 
-let clickCounter = 0;
+const imageOne = "assets/eab8104df5b39b69e868de47675737c1.jpg";
+const imageTwo = "assets/kisspng-bulbasaur-portable-network-graphics-image-ivysaur-1713906417665.webp";
+const imageThree = "assets/squirtle.jpeg";
+
+
+let characterCounter = 1;
 
 myButton.onclick = clickFunction
 
 function clickFunction() {
+    
 
-clickCounter = clickCounter + 1;
+    console.log("The button has been clicked")
+    
+    if(characterCounter > 3) {
+        characterCounter =1;
+    }
 
-    myParagraph.innerHTML = "I have been clicked " + clickCounter + " times"
+    myParagraph.innerHTML = "I am on character " + characterCounter
+
+    if(characterCounter === 1) {
+        characterImg.src = imageOne;
+        return;
+    }
+
+    if(characterCounter === 2) {
+        characterImg.src = imageTwo;
+        return;
+    }
+
+    if(characterCounter === 3) {
+        characterImg.src = imageThree;
+        return;
+    }
+    
+    characterCounterCounter = characterCounter + 1;
+    
+    // characterImg.src = "assets/kisspng-bulbasaur-portable-network-graphics-image-ivysaur-1713906417665.webp"
+    
 }
+
