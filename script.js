b6e7955b15fb214560b325b1dde4c31d034aa88c
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2024-10-15 11:32:40 +0100

    last before cleanup

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const imageSources = ["assets/charzard.png", "assets/bulbasaur.png", "assets/squ
 const myButton = document.getElementById("button")
 const myParagraph = document.getElementById("my-paragraph")
 const characterImg = document.getElementById("character-img")
+const customName = document.getElementById("name")
+const nameButton = document.getElementById("nameButton")
+
 
 // const imageOne = "assets/charzard.png";
 // const imageTwo = "assets/bulbasaur.png";
@@ -20,11 +23,17 @@ const nameSources = ["Charzard", "Bulbasaur", "Squirtle"]
 // const nameTwo = "Bulbasaur";
 // const nameThree = "Squirtle";
 
+nameButton.onclick = nameChangeFunction
+
+function nameChangeFunction() {
+    myParagraph.innerHTML = customName.value
+}
+
 let characterCounter = 1;
 
 myButton.onclick = clickFunction
 
-myParagraph.innerHTML = nameOne
+// myParagraph.innerHTML = nameOne
 
 function clickFunction() {
     characterCounter = characterCounter + 1;
@@ -35,28 +44,28 @@ function clickFunction() {
         characterCounter =1;
     }
 
-    myParagraph.innerHTML = "I am on character " + characterCounter;
 
-    if(characterCounter === 1) {
-        characterImg.src = imageSources [characterCounter - 1];
-        characterImg.alt = altSources [characterCounter - 1];
-        myParagraph.innerHTML = nameSources [characterCounter - 1];
-        return;
-    }
 
-    if(characterCounter === 2) {
-        characterImg.src = imageSources [1];
-        characterImg.alt = altSources [1];
-        myParagraph.innerHTML = nameSources [1];
-        return;
-    }
+    
+        characterImg.src = imageSources[characterCounter - 1];
+        characterImg.alt = altSources[characterCounter - 1];
+        myParagraph.innerHTML = nameSources[characterCounter - 1];
 
-    if(characterCounter === 3) {
-        characterImg.src = imageSources [2];
-        characterImg.alt = altSources [2];
-        myParagraph.innerHTML = nameSources [2];
-        return;
-    }
+    
+
+    // if(characterCounter === 2) {
+    //     characterImg.src = imageSources [1];
+    //     characterImg.alt = altSources [1];
+    //     myParagraph.innerHTML = nameSources [1];
+    //     return;
+    // }
+
+    // if(characterCounter === 3) {
+    //     characterImg.src = imageSources [2];
+    //     characterImg.alt = altSources [2];
+    //     myParagraph.innerHTML = nameSources [2];
+    //     return;
+    // }
     
     
     // characterImg.src = "assets/kisspng-bulbasaur-portable-network-graphics-image-ivysaur-1713906417665.webp"
