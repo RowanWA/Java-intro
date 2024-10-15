9a8cb3e7a4cab16d1d4515128cedcc5b927d54ff
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2024-10-15 11:35:43 +0100

    tidy

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-const imageSources = ["assets/charzard.png", "assets/bulbasaur.png", "assets/squirtle.png"]
-
 const myButton = document.getElementById("button")
 const myParagraph = document.getElementById("my-paragraph")
 const characterImg = document.getElementById("character-img")
 const customName = document.getElementById("name")
 const nameButton = document.getElementById("nameButton")
 
-
-// const imageOne = "assets/charzard.png";
-// const imageTwo = "assets/bulbasaur.png";
-// const imageThree = "assets/squirtle.png";
+const imageSources = ["assets/charzard.png", "assets/bulbasaur.png", "assets/squirtle.png"]
 
 const altSources = ["Charzard", "Bulbasaur", "Squirtle"]
 
-// const altOne = "Charzard";
-// const altTwo = "Bulbasaur";
-// const altThree = "Squirtle";
-
 const nameSources = ["Charzard", "Bulbasaur", "Squirtle"]
 
-// const nameOne = "Charzard";
-// const nameTwo = "Bulbasaur";
-// const nameThree = "Squirtle";
 
 nameButton.onclick = nameChangeFunction
 
@@ -33,42 +21,18 @@ let characterCounter = 1;
 
 myButton.onclick = clickFunction
 
-// myParagraph.innerHTML = nameOne
 
 function clickFunction() {
     characterCounter = characterCounter + 1;
 
     console.log("The button has been clicked");
-    
-    if(characterCounter > 3) {
-        characterCounter =1;
+
+    if (characterCounter > 3) {
+        characterCounter = 1;
     }
 
-
-
-    
-        characterImg.src = imageSources[characterCounter - 1];
-        characterImg.alt = altSources[characterCounter - 1];
-        myParagraph.innerHTML = nameSources[characterCounter - 1];
-
-    
-
-    // if(characterCounter === 2) {
-    //     characterImg.src = imageSources [1];
-    //     characterImg.alt = altSources [1];
-    //     myParagraph.innerHTML = nameSources [1];
-    //     return;
-    // }
-
-    // if(characterCounter === 3) {
-    //     characterImg.src = imageSources [2];
-    //     characterImg.alt = altSources [2];
-    //     myParagraph.innerHTML = nameSources [2];
-    //     return;
-    // }
-    
-    
-    // characterImg.src = "assets/kisspng-bulbasaur-portable-network-graphics-image-ivysaur-1713906417665.webp"
-    
+    characterImg.src = imageSources[characterCounter - 1];
+    characterImg.alt = altSources[characterCounter - 1];
+    myParagraph.innerHTML = nameSources[characterCounter - 1];
 }
 
